ea57f957b8e23516a134ea1eb17c3a89ebf2a7c7
Ryan Franks <ryan.ethan.franks@gmail.com>
2023-12-06 19:36:07 +1300

    Fixing image issues

--- src/components/CountryDataBox/CountryDataBox.tsx
+++ src/components/CountryDataBox/CountryDataBox.tsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import styles from './CountryDataBox.module.scss';
-import {goToUrl, multiCSSHandler} from "../../utils/map.utils";
+import {formatPopCount, goToUrl, multiCSSHandler} from "../../utils/map.utils";
 import {CountryResponse} from "../../utils/types";
 import {useLanguageSelection} from "../../providers/LanguageStore.provider";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 import testImage from './germany-test-photo.jpg';
+import {log} from "util";
 
 interface CountryDataBoxProps {
     countryData: CountryResponse
@@ -38,13 +39,20 @@ const CountryDataBox = (props: CountryDataBoxProps) => {
         setPhotoUrl('');
     }, [languageStore.selectedLanguage]);
 
+    const downloadPhoto = (download_location: string) => {
+        const url = `${download_location}&client_id=${accessToken}`;
+        fetch(url, {
+            method: 'GET'
+        }).then();
+    }
+
     const getPhotoUrl = async () => {
-        const url = `https://api.unsplash.com/photos/random?query=${props.countryData.name.common}&client_id=${accessToken}`;
+        const url = `https://api.unsplash.com/photos/random?query=${props.countryData.name.nativeName}&client_id=${accessToken}`;
         const response = await fetch(url, {
             method: 'GET'
         });
 
-        const dataUrl = response.json().then(
+        response.json().then(
             data => {
                 if (data) {
                     setPhotoUrl(data.urls.small);
@@ -52,22 +60,12 @@ const CountryDataBox = (props: CountryDataBoxProps) => {
                        name: data.user.name,
                        url: data.user.links.html
                     });
+                    downloadPhoto(data.links.download_location);
                 }
             }
         ).catch(() => {
-            console.log('oops')
+            console.error('Could not fetch image');
         });
-    }
-
-    const formatPopCount = (pop: number) => {
-        if (pop > 1.0e+6 && pop < 1.0e+8) {
-            return (Math.abs(Number(props.countryData.population)) / 1.0e+6).toFixed(2) + "M";
-        } else if (pop < 1.0e+6) {
-            return (Math.abs(Number(props.countryData.population)) / 1.0e+3).toFixed(2) + "K";
-        } else if (pop > 1.0e+8) {
-            return (Math.abs(Number(props.countryData.population)) / 1.0e+9).toFixed(2) + "B";
-        }
-
     }
 
     return (
@@ -86,13 +84,11 @@ const CountryDataBox = (props: CountryDataBoxProps) => {
                         <img className={styles['cover']} src={photoUrl} alt={'not loaded'}/>
                         {
                             imageCredits ?
-                                <div className={styles['credits-container']} onClick={() => goToUrl(imageCredits.url)}>
-                                    {
-                                        "Photo by " + imageCredits?.name
-                                    }
-                                </div> : null
+                                    <a className={styles['credits-container']} href={imageCredits.url}>
+                                        Photo by {imageCredits?.name} on Unsplash
+                                    </a>
+                                 : null
                         }
-
                     </div>:
                     <div className={styles['spinner']}>
                         <LoadingSpinner  />
